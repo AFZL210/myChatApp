42eac3efb4c8d43b8d6d92ab347a7aff151e59d8
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-04-27 17:10:54 +0530

    initiate connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,33 @@
-console.log("test")
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const app: Application = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173/",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+    })
+})
+
+app.get("/test", (req: Request, res: Response) => {
+    res.json("server is running fine").status(200);
+})
+
+server.listen(5000, () => {
+    console.log("server running on port 5000");
+})
