53c3073333db6c943fbfaf090c8ecc782b049264
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-04-28 00:02:10 +0530

    update message box and fix scroll

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { io } from "socket.io-client";
 import Chat from './components/Chat';
+import "./App.css";
 
 const socket = io("ws://localhost:5000")
 
@@ -8,9 +9,11 @@ const App: React.FC = () => {
 
   const [username, setUsername] = useState<string>("");
   const [room, setRoom] = useState<number>(0);
+  const [joinedRoom, setJoinedRoom] = useState<Boolean>(false);
 
   const handleJoinRoom = (): void => {
     if (username !== "" && room !== 0) {
+      setJoinedRoom(true);
       socket.emit("join_room", room);
     } else {
       alert("username or room code cannot be empty");
@@ -18,13 +21,17 @@ const App: React.FC = () => {
   }
 
   return (
-    <div>
-      <h4>Join Chat</h4>
-      <input type='text' placeholder='Name' value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input type="number" placeholder="Room Code" onChange={(e) => setRoom(Number(e.target.value))} />
-      <button onClick={handleJoinRoom}>Join Room</button>
+    <div className='App'>
+      <div className='joinChatContainer'>
 
-      <Chat socket={socket} username={username} room={room} />
+        {!joinedRoom && <>
+          <h4>Join Chat</h4>
+          <input type='text' placeholder='Name' value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input type="number" placeholder="Room Code" onChange={(e) => setRoom(Number(e.target.value))} />
+          <button onClick={handleJoinRoom}>Join Room</button>
+        </>}
+        {joinedRoom && <Chat socket={socket} username={username} room={room} />}
+      </div>
     </div>
   )
 }
--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { Socket } from 'socket.io-client'
+import React, { useState, useEffect } from 'react';
+import { Socket } from 'socket.io-client';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 interface Props {
     socket: Socket
@@ -17,6 +18,7 @@ interface messageInterface {
 const Chat: React.FC<Props> = ({ socket, username, room }) => {
 
     const [currentMessage, setCurrentMessage] = useState<string>("");
+    const [messageList, setMessageList] = useState<messageInterface[]>([]);
 
     const sendMessage = async (): Promise<void> => {
         if (currentMessage !== "") {
@@ -28,17 +30,42 @@ const Chat: React.FC<Props> = ({ socket, username, room }) => {
             }
 
             await socket.emit("send_message", messageData);
+            setMessageList([...messageList, messageData]);
+            setCurrentMessage("")
         }
     }
 
+    useEffect(() => {
+        socket.on("recieve_message", (data:any):void => {
+            console.log(data);
+            setMessageList([...messageList, data]);
+        })
+    }, [socket])
+
     return (
-        <div>
+        <div className='chat-window'>
             <div className='chat-header'>
                 <p>Live Chat</p>
             </div>
-            <div className='chat-body'></div>
+            <div className='chat-body'>
+                <ScrollToBottom className='message-container'>
+                {messageList.map((message) => {
+                    return (<div className='message' id={username===message.author?"you":"other"}>
+                        <div>
+                            <div className='message-content'>
+                                <p>{message.message}</p>
+                            </div>
+                            <div className='message-meta'>
+                                <p id='time'>{message.time}</p>
+                                <p id='author'>{message.author}</p>
+                            </div>
+                        </div>
+                    </div>)
+                })}
+                </ScrollToBottom>
+            </div>
             <div className='chat-footer'>
-                <input type="text" placeholder='Hey!' value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} />
+                <input style={{height: "60%", border: "0px"}} type="text" placeholder='Hey!' value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} />
                 <button onClick={sendMessage}>&#9658;</button>
             </div>
         </div>
