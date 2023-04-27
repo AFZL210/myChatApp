9a59a094fb2a1b3f405100e0c859f170fc26430f
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-04-27 21:24:23 +0530

    handle join room event

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,16 +11,25 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173/",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"]
     }
 })
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
+    console.log(`user ${socket.id} connected`);
+
+    socket.on("join_room", (data: any) => {
+        socket.join(data);
+        console.log(`user with ID ${socket.id} joined room: ${data}`)
+    })
+
+    socket.on("send_message", (data: any) => {
+        console.log(data)
+    })
 
     socket.on("disconnect", () => {
-        console.log("user disconnected", socket.id);
+        console.log(`user ${socket.id} disconnected`);
     })
 })
 
